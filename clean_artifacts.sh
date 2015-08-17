5f6065b00361ed084bb181d66333c64929e49747
Andrew Adams <andrew.b.adams@gmail.com>
2015-08-17 13:01:43 -0700

    Add script to clean up artifacts dir

---
+++ clean_artifacts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Deletes builds where a newer one exists. The first line finds all
+# the build names (e.g. halide-mac-64-trunk) by stripping out the
+# commit hash. The second line lists all but the newest of each build
+# prefix. The third line deletes them.
+
+ls halide-*.tgz | sed "s/-[^-]*.tgz//" | sort | uniq | \
+while read B; do ls -tr ${B}*.tgz | tail -n +2; done | \
+while read F; do rm ${F}; done
