e9e227c5959c32caab078e9dffbd4bb0914b5b17
Steven Johnson <srj@google.com>
2021-04-01 11:43:44 -0700

    Create activate_windows_venv.sh (#173)

---
+++ activate_windows_venv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Windows Python prepends a Windows-style path when you activate a venv;
+# this is fine in cmd.exe but confuses CMake greatly in git-bash.
+# Dance on PATH to fix this.
+#
+# Note: execute via `source activate_windows_venv.sh`, not by running directly.
+#
+. venv/Scripts/activate
+export PATH=$(echo $PATH | sed 's,^\([C-Gc-g]\):\\,/\1/,g' | sed 's,:\([C-Gc-g]\):\\,/\1/,g')
