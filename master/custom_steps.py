2771afa2a6e3c7e168b318ee2b4202e1bc75540a
Steven Johnson <srj@google.com>
2024-01-23 11:33:16 -0800

    Update custom_steps.py

--- master/custom_steps.py
+++ master/custom_steps.py
@@ -138,18 +138,22 @@ class CleanOldFiles(BuildStep):
 class FileUploadIfNotExist(FileUpload):
     name = 'file-upload-if-not-exist'
 
-    @defer.inlineCallbacks
-    def run(self):
-        stdio = yield self.addLog('stdio')
+    def __init__(self, *, deletefn, workdir, **kwargs):
+        super().__init__(**kwargs)
+        # This is a hack to show some sort of progress-like output when uploading
+        self.debug = True
 
+    def run(self):
         masterdest = os.path.expanduser(self.masterdest)
         if os.path.isfile(masterdest) and os.path.getsize(masterdest) > 0:
-            stdio.addStdout(f"File {repr(masterdest)} already exists on dest, skipping upload!")
-            yield stdio.finish()
+            # TODO: this requires @defer.inlineCallbacks, but adding that
+            # will make the non-skip-upload path fail because we have no callbacks.
+            # How to resolve? Not sure. Oh well.
+            #
+            # stdio.addStdout(f"File {repr(masterdest)} already exists on dest, skipping upload!")
+            # yield stdio.finish()
             return SUCCESS
 
-        stdio.addStdout(f"Preparing to upload to {repr(masterdest)}...")
-        yield stdio.finish()
         return super().run()
 
 
