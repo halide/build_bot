dd8ae05c099fb06f9266318ddf8586ac82e75956
Steven Johnson <srj@google.com>
2024-01-23 16:30:34 -0800

    Update custom_steps.py

--- master/custom_steps.py
+++ master/custom_steps.py
@@ -141,12 +141,15 @@ class FileUploadIfNotExist(FileUpload):
     def run(self):
         masterdest = os.path.expanduser(self.masterdest)
         if os.path.isfile(masterdest) and os.path.getsize(masterdest) > 0:
-            stdio = yield self.addLog('stdio')
-            stdio.addStdout(f"File {repr(masterdest)} already exists on dest, skipping upload!")
-            yield stdio.finish()
+            # TODO: this requires @defer.inlineCallbacks, but adding that
+            # will make the non-skip-upload path fail because we have no callbacks.
+            # How to resolve? Not sure. Oh well.
+            #
+            # stdio.addStdout(f"File {repr(masterdest)} already exists on dest, skipping upload!")
+            # yield stdio.finish()
             return SUCCESS
 
-        yield from super().run()
+        return super().run()
 
 
 class CTest(ShellMixin, CompositeStepMixin, BuildStep):
