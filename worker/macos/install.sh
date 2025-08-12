ce84dcdbb533c2fa3dc99b78b1872aab500149c6
Alex Reinking <areinking@adobe.com>
2025-08-12 11:59:56 -0400

    Stash WASM/WGPU environment in launchd.

--- worker/macos/install.sh
+++ worker/macos/install.sh
@@ -21,13 +21,41 @@ fi
 # Install Homebrew dependencies
 
 brew update
-brew install --quiet ccache doxygen gettext libjpeg libpng protobuf uv
+brew install --quiet ccache doxygen gettext libjpeg libpng node@18 protobuf uv
 
 ##
 # Configure ccache
 
 ccache --set-config=sloppiness=pch_defines,time_macros
 ccache -M 20G
+
+##
+# Detect Dawn dependency
+
+# TODO: this needs to be removed when WebGPU support is brought up to date with
+#   upstream Dawn and https://github.com/halide/Halide/pull/8714 can be merged.
+#   See also: https://github.com/halide/build_bot/issues/311
+
+echo "Persisting WASM/WGPU environment to launchd agent..."
+
+if [ -z "$HL_WEBGPU_NODE_BINDINGS" ]; then
+  fail "Environment variable HL_WEBGPU_NODE_BINDINGS unset: cannot continue"
+fi
+export HL_WEBGPU_NODE_BINDINGS
+
+if [ -z "$HL_WEBGPU_NATIVE_LIB" ]; then
+  fail "Environment variable HL_WEBGPU_NATIVE_LIB unset: cannot continue"
+fi
+export HL_WEBGPU_NATIVE_LIB
+
+if [ -z "$EMSDK" ]; then
+  fail "Environment variable EMSDK unset: cannot continue"
+fi
+export EMSDK
+
+# TODO: this should be managed in-repo
+HALIDE_NODE_JS_PATH="$(brew --prefix node@18)/bin/node"
+export HALIDE_NODE_JS_PATH
 
 ##
 # Install the autostart script
@@ -42,7 +70,7 @@ if [ -z "$HALIDE_BB_WORKER_NAME" ]; then
 fi
 export HALIDE_BB_WORKER_NAME
 
-LAUNCHD_PATH="$(dirname "$(command -v uv)"):/usr/bin:/bin:/usr/sbin:/sbin"
+LAUNCHD_PATH="$(brew --prefix)/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 export LAUNCHD_PATH
 
 envsubst < worker/macos/org.halide-lang.buildbot.plist.in > "${PLIST}"
