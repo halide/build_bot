3fa26bc0288449cbce6b5e9efe7b3b9a136ba19d
Alex Reinking <areinking@adobe.com>
2025-08-21 15:02:24 -0400

    Install libjpeg-turbo rather than libjpeg

--- worker/macos/install.sh
+++ worker/macos/install.sh
@@ -21,7 +21,7 @@ fi
 # Install Homebrew dependencies
 
 brew update
-brew install --quiet ccache doxygen gettext libjpeg libpng node@18 pkg-config protobuf uv
+brew install --quiet ccache doxygen gettext libjpeg-turbo libpng node@18 pkg-config protobuf uv
 
 ##
 # Configure ccache
